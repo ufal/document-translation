709ca6846f6e23384026f28d02164da0e2e01737
Jirka Balhar <balhar.j@gmail.com>
2024-02-25 19:10:34 +0100

    translation using cuni translator

--- translate.py
+++ translate.py
@@ -1,22 +1,31 @@
 import requests, sys
 
-url = "https://translator.cuni.cz/api/v2/languages/"
-for line in sys.stdin:
-    r = requests.post(
-        url,
-        data={
+def translate_text(input_text):
+    url = "https://translator.cuni.cz/api/v2/languages"
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+    data = {
             "src": "cs",
             "tgt": "uk",
-            "input_text": line,
+            "input_text": input_text,
             "logInput": "false",
             "author": "H-edu",
-        },
-        headers={
-            "accept": "application/json",
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-    )
-    #    print(r.json())
-    sys.stderr.write(str(r.json()))
-    sentences = " ".join(r.json()).replace("\n", "")
-    print(sentences)
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+
+    if response.status_code == 200:
+        response = response.json()
+        sentences = " ".join(response).replace("\n", "")
+        return sentences
+    else:
+        print(f"Error: {response.status_code}");
+        print(response.text)
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        translation = translate_text(line)
+        sys.stderr.write(str(translation))
+        print(translation)
